895434b9f8534879da9c2b8faba9c01fca5a3eef
Micah Banschick <micha.ban@gmail.com>
2020-12-05 22:03:24 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :category, inclusion: { in: :correct_category?}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validates :content, length: { minimum: 100 }
 
   def correct_category?
