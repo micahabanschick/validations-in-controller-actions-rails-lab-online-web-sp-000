9e3468784b1eac94702b30ec719068882652b842
Micah Banschick <micha.ban@gmail.com>
2020-12-05 21:24:12 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, if: :correct_category?
+
+  def correct_category?
+    category == "Fiction" || category == "Non-Fiction"
+  end
 end
