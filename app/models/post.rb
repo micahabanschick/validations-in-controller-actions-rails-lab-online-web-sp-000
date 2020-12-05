7eccdc5f2aacb3efa04d1ec169245ffbd3251da1
Micah Banschick <micha.ban@gmail.com>
2020-12-05 21:43:01 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, presence: true, if: :correct_category?
-  validates :content, length: { minimum: 2 }
+  validates :content, length: { minimum: 100 }
 
   def correct_category?
     category == "Fiction" || category == "Non-Fiction"
